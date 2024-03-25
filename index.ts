ecbdf858c2a55dfc0cdb32d1035eb4d8fdfc6e6a
hafsakanwal601 <164800362+hafsakanwal601@users.noreply.github.com>
2024-03-26 01:52:59 +0500

    push my cli guessing game to github

---
+++ index.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const systemGeneratedNo=Math.floor( Math.random()*10+1)
+const answers = await inquirer.prompt([
+    {
+        type:"number",
+        name:"userGuess",
+        message: (chalk.red.bgBlue ("write your guess"))
+    },
+])
+
+if (answers.userGuess == Number){
+    console.log (chalk.black.bgYellow("you win"));
+}  
+else {
+    console.log( chalk.red.bgBlue("please try again"));
+}
